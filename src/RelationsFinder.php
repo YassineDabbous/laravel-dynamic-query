27e49544ea599ae97222659f359d3d6c4934a04e
yassine dabbous <yassine.dabbous@gmail.com>
2026-02-21 17:37:34 +0100

    add cache

--- src/RelationsFinder.php
+++ src/RelationsFinder.php
@@ -3,6 +3,17 @@
 namespace YassineDabbous\DynamicQuery;
 
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Collection;
 use ReflectionClass;
 use ReflectionMethod;
 use ReflectionNamedType;
@@ -10,19 +21,29 @@ use ReflectionUnionType;
 
 trait RelationsFinder
 {
+    /** @var array<string, array> Cache for reflection results */
+    protected static array $__dynamicQueryRelationCache = [];
+
 
     /** 
      * Guess relations from methods
      * @return array<string>
      */
-    public function guessDynamicRelations(): array
+    protected function guessDynamicRelations(): array
     {
-        $class = new ReflectionClass($this);
+        $class = static::class;
+        if (isset(static::$__dynamicQueryRelationCache[$class])) {
+            return static::$__dynamicQueryRelationCache[$class];
+        }
 
-        return collect($class->getMethods())
+        $reflection = new ReflectionClass($this);
+
+        $result = Collection::make($reflection->getMethods())
             ->filter(fn (ReflectionMethod $method) => $this->hasReturnType($method, Relation::class))
             ->mapWithKeys(fn (ReflectionMethod $method) => [$method->getName() => $this->guessRelationColumns($method)])
             ->toArray();
+
+        return static::$__dynamicQueryRelationCache[$class] = $result;
     }
 
 
@@ -33,13 +54,13 @@ trait RelationsFinder
             return false;
         }
 
-        if ($method->getReturnType() instanceof ReflectionNamedType) {
+        if (is_a($method->getReturnType(), ReflectionNamedType::class)) {
             $returnType = $method->getReturnType()->getName();
 
             return is_a($returnType, $class, true);
         }
 
-        if ($method->getReturnType() instanceof ReflectionUnionType) {
+        if (is_a($method->getReturnType(), ReflectionUnionType::class)) {
             foreach ($method->getReturnType()->getTypes() as $type) {
                 $returnType = $type->getName();
 
@@ -57,16 +78,16 @@ trait RelationsFinder
     {
         $relation = $method->invoke($this);
         return match (true) {
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\MorphOne::class) =>  $relation->getLocalKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\MorphMany::class) => $relation->getLocalKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\MorphTo::class) =>     [$relation->getMorphType(), $relation->getForeignKeyName()],
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\MorphToMany::class) => [$relation->getMorphType(), $relation->getForeignKeyName()],
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\HasOne::class) => $relation->getLocalKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\HasMany::class) => $relation->getLocalKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\HasOneThrough::class) => $relation->getLocalKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\HasManyThrough::class) => $relation->getLocalKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\BelongsTo::class) => $relation->getForeignKeyName(),
-            is_a($relation, \Illuminate\Database\Eloquent\Relations\BelongsToMany::class) => $relation->getParentKeyName(),
+            is_a($relation, MorphOne::class) =>  $relation->getLocalKeyName(),
+            is_a($relation, MorphMany::class) => $relation->getLocalKeyName(),
+            is_a($relation, MorphTo::class) =>     [$relation->getMorphType(), $relation->getForeignKeyName()],
+            is_a($relation, MorphToMany::class) => [$relation->getMorphType(), $relation->getForeignKeyName()],
+            is_a($relation, HasOne::class) => $relation->getLocalKeyName(),
+            is_a($relation, HasMany::class) => $relation->getLocalKeyName(),
+            is_a($relation, HasOneThrough::class) => $relation->getLocalKeyName(),
+            is_a($relation, HasManyThrough::class) => $relation->getLocalKeyName(),
+            is_a($relation, BelongsTo::class) => $relation->getForeignKeyName(),
+            is_a($relation, BelongsToMany::class) => $relation->getParentKeyName(),
             default => null,
         };
     }
